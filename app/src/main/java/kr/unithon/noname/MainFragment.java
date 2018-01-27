9a06aa0de43d34a73e4821002d228d5e28b35bd9
jjangjamin <jaeminim95@gmail.com>
2018-01-27 18:05:33 +0900

    입니다

--- app/src/main/java/kr/unithon/noname/MainFragment.java
+++ app/src/main/java/kr/unithon/noname/MainFragment.java
@@ -1,8 +1,10 @@
 package kr.unithon.noname;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by user on 2018-01-27.
  */
 
-public class MainFragment {
+public class MainFragment extends Fragment {
 }
