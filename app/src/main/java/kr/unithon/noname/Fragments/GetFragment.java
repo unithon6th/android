154cd0e5f3e8192294a6e253f69a7134b4d04960
jjangjamin <jaeminim95@gmail.com>
2018-01-28 00:44:43 +0900

    분양프래그먼트 작업중

---
+++ app/src/main/java/kr/unithon/noname/Fragments/GetFragment.java
@@ -0,0 +1,28 @@
+package kr.unithon.noname.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 2018-01-27.
+ */
+
+public class GetFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+}
