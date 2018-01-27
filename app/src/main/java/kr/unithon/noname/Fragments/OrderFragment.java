0addd746fbb023a5b3f8bb27841fc3eb124762c0
jjangjamin <jaeminim95@gmail.com>
2018-01-28 05:47:55 +0900

    분양프래그먼트

--- app/src/main/java/kr/unithon/noname/Fragments/OrderFragment.java
+++ app/src/main/java/kr/unithon/noname/Fragments/OrderFragment.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import kr.unithon.noname.R;
@@ -18,8 +21,13 @@ import kr.unithon.noname.R;
  * Created by user on 2018-01-28.
  */
 
-public class OrderFragment extends Fragment {
+public class OrderFragment extends Fragment implements View.OnClickListener {
     private Context mContext;
+    Button btnplus, btnminus;
+    TextView quantity;
+    TextView quantity2;
+    int counter = 0;
+
 Spinner typesofVeges, optionsVeges;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -66,6 +74,31 @@ String[] veges ={"배추", "토마토", "감귤", "오이", "피망", "양파"};
 
             }
         });
+
+        btnplus = (Button)rootView.findViewById(R.id.plus);
+        btnminus = (Button)rootView.findViewById(R.id.minus);
+        quantity = (TextView)rootView.findViewById(R.id.textView8);
+        quantity2 = (TextView)rootView.findViewById(R.id.textView9);
+        btnplus.setOnClickListener(this);
+        btnminus.setOnClickListener(this);
+
+        quantity.setTextSize(TypedValue.COMPLEX_UNIT_SP,24);
+        quantity2.setTextSize(TypedValue.COMPLEX_UNIT_SP,24);
         return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==btnplus){
+            counter+=5;
+            quantity.setText(Integer.toString(counter));
+            quantity2.setText(Integer.toString(counter));
+        }
+        if(view==btnminus){
+            counter-=5;
+            quantity.setText(Integer.toString(counter));
+            quantity2.setText(Integer.toString(counter));
+        }
+
     }
 }
