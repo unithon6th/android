2906d1538719ad1ccc9903f21f680bce9b7d4fce
JSpiner <jspiner@naver.com>
2018-01-27 15:57:14 +0900

    로티 라이브러리 추가, BaseFragment/CustomView 설정

--- app/src/main/java/kr/unithon/noname/ui/base/BaseActivity.java
+++ app/src/main/java/kr/unithon/noname/ui/base/BaseActivity.java
@@ -1,10 +1,37 @@
 package kr.unithon.noname.ui.base;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.CallSuper;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
+
+    private Handler uiHandler;
+
+    @CallSuper
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uiHandler = new Handler();
+    }
+
+    protected void post(Runnable runnable) {
+        uiHandler.post(runnable);
+    }
+
+    protected void postDelayed(Runnable runnable, long delayMillis) {
+        uiHandler.postDelayed(runnable, delayMillis);
+    }
+
+    protected void showToast(String text) {
+        Toast.makeText(
+                getBaseContext(),
+                text,
+                Toast.LENGTH_LONG
+        ).show();
+    }
 
 }
--- app/src/main/java/kr/unithon/noname/ui/crop/detail/CropDetailActivity.java
+++ app/src/main/java/kr/unithon/noname/ui/crop/detail/CropDetailActivity.java
@@ -1,8 +1,24 @@
 package kr.unithon.noname.ui.crop.detail;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import kr.unithon.noname.R;
 import kr.unithon.noname.ui.base.BaseActivity;
 
 public class CropDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop_detail);
+
+        init();
+    }
+
+    private void init() {
+
+    }
 
 
 }
