6c5c130f74b0f870b201a792b1afd34a4c045183
jjangjamin <jaeminim95@gmail.com>
2018-01-28 02:48:21 +0900

    no message

--- app/src/main/java/kr/unithon/noname/Fragments/GetFragment.java
+++ app/src/main/java/kr/unithon/noname/Fragments/GetFragment.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import kr.unithon.noname.R;
 
 /**
  * Created by user on 2018-01-27.
@@ -21,7 +26,34 @@ public class GetFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_get, container, false);
+
+        Spinner s = (Spinner)rootView.findViewById(R.id.nameofveges);
+        Spinner s2 = (Spinner)rootView.findViewById(R.id.optionsforveges);
+        s2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(), adapterView.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(), adapterView.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        return  rootView;
     }
 
 
